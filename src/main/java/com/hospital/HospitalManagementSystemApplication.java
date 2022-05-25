ba6211e738ecf093dc072e152f4073ed20ce3210
beneko <behzadnekouei@gmail.com>
2022-05-25 12:10:55 +0200

    generate and add some RDV to the database for evry Patient with a Medcin with the same ID

--- src/main/java/com/hospital/HospitalManagementSystemApplication.java
+++ src/main/java/com/hospital/HospitalManagementSystemApplication.java
@@ -1,18 +1,27 @@
 package com.hospital;
 
 import java.util.Date;
-
+import java.util.List;
 import java.util.stream.Stream;
+
+import javax.persistence.OneToMany;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.annotation.CreatedBy;
 
 import com.hospital.entities.Medecin;
 import com.hospital.entities.Patient;
+import com.hospital.entities.RendezVous;
+import com.hospital.entities.StatusRDV;
 import com.hospital.repositories.MedecinRepository;
 import com.hospital.repositories.PatientRepository;
+import com.hospital.repositories.RendezVousRepository;
+
+import antlr.CharQueue;
+import lombok.val;
 
 
 @SpringBootApplication
@@ -23,7 +32,7 @@ public class HospitalManagementSystemApplication{
 	}
 	
 	@Bean
-	CommandLineRunner start(PatientRepository patientRepository, MedecinRepository medecinRepository) {
+	CommandLineRunner start(PatientRepository patientRepository, MedecinRepository medecinRepository, RendezVousRepository rendezVousRepository) {
 		
 		return args -> {
 			
@@ -42,6 +51,17 @@ public class HospitalManagementSystemApplication{
 				medecin.setSpecialite(Math.random()<0.5 ?"Generaliste" : "Specialiste");
 				medecinRepository.save(medecin);
 			});
+			
+			//Ici, je vais créer un rdv pour chaque patient avec un medcin avec le meme id
+			patientRepository.findAll().forEach(patient ->{
+				RendezVous rendezVous= new RendezVous();
+				rendezVous.setPatient(patient);
+				rendezVous.setMedecin(medecinRepository.findById(patient.getId()).get());
+				rendezVous.setDate(new Date());
+				rendezVous.setStatus(StatusRDV.PENDING);
+				rendezVousRepository.save(rendezVous);
+			});
+			
 			
 		};
 	}
